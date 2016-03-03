10e14a35cd2e04d96271e79188edd616cc6eeb8f
Tran Quang Trung <tran.quang.trung@framgia.com>
2016-03-03 10:45:23 +0700

    Finish task filter word

--- app/controllers/words_controller.rb
+++ app/controllers/words_controller.rb
@@ -1,7 +1,14 @@
 class WordsController < ApplicationController
+  before_action :logged_in_user, only: [:index]
 
   def index
     @categories = Category.all
-    @words = Word.paginate page: params[:page]
+    @words = if params[:category_id].blank?
+      Word.send (params[:option] ||= "all_words"), current_user.id
+    else
+      @category = Category.find_by id: params[:category_id]
+      @category.words.send (params[:option] ||= "all_words"), current_user.id
+    end
+    @words = @words.paginate page: params[:page]
   end
 end
