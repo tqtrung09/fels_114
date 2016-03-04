ce22a4aadf4fd63f0dea68db4e442b6672bf2d52
Tran Quang Trung <tran.quang.trung@framgia.com>
2016-03-04 15:42:03 +0700

    Finish task add word jquery

--- app/models/word.rb
+++ app/models/word.rb
@@ -8,6 +8,7 @@ class Word < ActiveRecord::Base
   mount_uploader :sound, SoundUploader
 
   validates :content, presence: true, length: {maximum: 50}
+  validate :just_only_correct_answer
 
   QUERRY_WORD_LEARNDED = "id in (select lesson_words.word_id from
     lesson_words join lessons on lesson_words.lesson_id = lessons.id
@@ -20,4 +21,10 @@ class Word < ActiveRecord::Base
   scope :all_words, -> user_id{}
   scope :learned, -> user_id{where QUERRY_WORD_LEARNDED, user_id}
   scope :not_learned, -> user_id{where QUERRY_WORD_NOT_LEARNDED, user_id}
+
+  private
+  def just_only_correct_answer
+    errors.add :base, I18n.t("words.validate_correct") unless
+      answers.select{|ans| ans.correct}.size == 1
+  end
 end
